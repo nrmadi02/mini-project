fc7d88907e120998080836d7cd708a8d6bd9dcdd
Nur Ahmadi <ahmadinur2202@gmail.com>
2022-05-14 19:02:32 +0800

    update error env file

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
 	"github.com/nrmadi02/mini-project/app"
 	"github.com/nrmadi02/mini-project/app/config"
 	"github.com/nrmadi02/mini-project/app/utils"
@@ -10,11 +9,6 @@ import (
 
 func init() {
 	log.SetFormatter(&log.JSONFormatter{})
-
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	
 	connectMongo, err := config.ConnectMongo()
 	if err != nil {
